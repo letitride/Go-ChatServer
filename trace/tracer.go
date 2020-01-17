73c4ffc304e332bd11b98839eddd0491ccd47f46
letitride <ichikawa.fumiya@gmail.com>
2020-01-17 20:33:01 +0900

    traceパッケージの実装

--- trace/tracer.go
+++ trace/tracer.go
@@ -2,13 +2,25 @@ package trace
 
 import "io"
 
+import "fmt"
+
 // Tracer はログ記録できるオブジェクトのインターフェース
 type Tracer interface {
 	//...interface{}は任意の型の引数を0個以上受け取れる
 	Trace(...interface{})
 }
 
-// New はメソッド
+type tracer struct {
+	out io.Writer
+}
+
+// Tracerインターフェースの実装
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+// New はインターフェースTracerを返すメソッド
 func New(w io.Writer) Tracer {
-	return nil
+	return &tracer{out: w}
 }
