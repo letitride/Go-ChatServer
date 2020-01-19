87c8e15e46a09d1503fb27755505e184c9dbb36a
letitride <ichikawa.fumiya@gmail.com>
2020-01-19 11:09:38 +0900

    認証プロバイダからcallbackアクセスの解釈

--- chat/auth.go
+++ chat/auth.go
@@ -1,14 +1,13 @@
 package main
 
-import "net/http"
-
-import "strings"
-
-import "log"
-
-import "fmt"
-
-import "github.com/stretchr/gomniauth"
+import (
+	"fmt"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -48,7 +47,37 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalln("GetBeginAuthURLの呼び出し中にエラーが発生しました:", provider, "-", err)
 		}
+		//認証プロバイダページへリダイレクト
 		w.Header().Set("Location", loginURL)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗しました:", provider, "-", err)
+		}
+
+		//r.URL.RawQuery GETパラメータ全体文字列
+		//callback時のproviderから渡されたパラメータから認証を完了させる
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("認証を完了できませんでした:", provider, "-", err)
+		}
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("ユーザの取得に失敗しました:", provider, "-", err)
+		}
+
+		//user名をname:usernameとしてbase64encode
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		//base64した文字列をauthというキー名でcookieに保存
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+		w.Header()["Location"] = []string{"/chat"}
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
