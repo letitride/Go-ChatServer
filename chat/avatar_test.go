24c3a0d0ad06d2098bd80d9157221e0a6bd72d1b
letitride <ichikawa.fumiya@gmail.com>
2020-01-29 09:37:02 +0900

    変数名testUrlをtestURLに変更

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -9,13 +9,13 @@ func TestAuthAvatar(t *testing.T) {
 	if err != ErrNoAvatarURL {
 		t.Error("値が存在しない場合、AuthAvatar.GetAvatarURLは" + "ErrNoAvatarURLを返すべきです。")
 	}
-	testUrl := "http://url-to-avatar/"
-	client.userData = map[string]interface{}{"avatar_url": testUrl}
+	testURL := "http://url-to-avatar/"
+	client.userData = map[string]interface{}{"avatar_url": testURL}
 	url, err = authAvatar.GetAvatarURL(client)
 	if err != nil {
 		t.Error("値が存在する場合、AuthAvatar.GetAvatarURLは" + "エラーを返すべきではありません。")
 	} else {
-		if url != testUrl {
+		if url != testURL {
 			t.Error("AuthAvatar.GetAvatarURLは正しいURLを返すべきです")
 		}
 	}
