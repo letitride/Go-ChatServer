26652a8753aac830d9ac871af658d88404d8758d
letitride <ichikawa.fumiya@gmail.com>
2020-04-06 22:24:14 +0900

    3.3.1 ユーザの識別

--- chat/auth.go
+++ chat/auth.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/objx"
+	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -67,8 +69,13 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("ユーザの取得に失敗しました:", provider, "-", err)
 		}
 
+		m := md5.New()
+		io.WriteString(m, strings.ToLower(user.Email()))
+		userID := fmt.Sprintf("%x", m.Sum(nil))
+
 		//user名をname:usernameとしてbase64encode
 		authCookieValue := objx.New(map[string]interface{}{
+			"userid":     userID,
 			"name":       user.Name(),
 			"avatar_url": user.AvatarURL(),
 			"email":      user.Email(),
