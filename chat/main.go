4c560f46867457aeb1b1d33f52b8680b284ae801
letitride <ichikawa.fumiya@gmail.com>
2020-07-04 14:07:21 +0900

    3.3.3 アップロードされたファイルの処理

--- chat/main.go
+++ chat/main.go
@@ -58,6 +58,7 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/uploader", uploaderHandler)
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
 			Name:   "auth",
